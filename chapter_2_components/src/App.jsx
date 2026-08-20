d4163ab49945b5278601c0a3693b5e8d92ead7bb
Sourabh Singh <saurav2004dangi@gmail.com>
2026-08-20 10:23:34 +0530

    React chapter_2_components

---
+++ chapter_2_components/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Card from './components/card'
+
+const App = () => {
+  return (
+    <div>
+      < Card />
+      < Card />
+      < Card />
+      < Card />
+
+    </div>
+  )
+}
+
+export default App
---
+++ chapter_2_components/src/components/card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    <div className='box'> 
+        <h2>Saurabh Singh</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+    </div>
+  )
+}
+
+export default card
